3c78099044a03f3162beaca03dfc868b10d00a6a
noame <noame@jfrog.com>
2026-03-12 22:57:25 +0200

    fix: zero-pad date to dd/mm format

--- public/shabbat-oref.js
+++ public/shabbat-oref.js
@@ -150,7 +150,9 @@ function normName(s) {
 }
 
 function dateStr(date) {
-  return `${date.getDate()}/${date.getMonth() + 1}`;
+  const d = String(date.getDate()).padStart(2, "0");
+  const m = String(date.getMonth() + 1).padStart(2, "0");
+  return `${d}/${m}`;
 }
 
 function timeStr(date) {
